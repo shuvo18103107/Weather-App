3723b616d7ed85eb9a88173cc1e087586887de7f
shuvo18103107 <18103107@iubat.edu>
2020-11-21 00:05:59 +0600

    🔥 Improving the UI, error correction, usability

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:io';
 
 void main() => runApp(WeatherApp());
 
@@ -10,23 +11,38 @@ class WeatherApp extends StatefulWidget {
 }
 
 class _WeatherAppState extends State<WeatherApp> {
-  int temperature = 0;
+  int temperature;
   String location = 'San Francisco';
   int woeid = 2487956;
   String weather = 'clear';
+  String abbreviation = '';
+  String errorMessage = '';
 
   String searchApiUrl =
       'https://www.metaweather.com/api/location/search/?query=';
   String locationApiUrl = 'https://www.metaweather.com/api/location/';
 
-  void fetchSearch(String input) async {
-    var searchResult = await http.get(searchApiUrl + input);
-    var result = json.decode(searchResult.body)[0];
+  initState() {
+    super.initState();
+    fetchLocation();
+  }
 
-    setState(() {
-      location = result["title"];
-      woeid = result["woeid"];
-    });
+  void fetchSearch(String input) async {
+    try {
+      var searchResult = await http.get(searchApiUrl + input);
+      var result = json.decode(searchResult.body)[0];
+
+      setState(() {
+        location = result["title"];
+        woeid = result["woeid"];
+        errorMessage = '';
+      });
+    } catch (error) {
+      setState(() {
+        errorMessage =
+            "Sorry, we don't have data about this city. Try another one.";
+      });
+    }
   }
 
   void fetchLocation() async {
@@ -38,12 +54,13 @@ class _WeatherAppState extends State<WeatherApp> {
     setState(() {
       temperature = data["the_temp"].round();
       weather = data["weather_state_name"].replaceAll(' ', '').toLowerCase();
+      abbreviation = data["weather_state_abbr"];
     });
   }
 
-  void onTextFieldSubmitted(String input) {
-    fetchSearch(input);
-    fetchLocation();
+  void onTextFieldSubmitted(String input) async {
+    await fetchSearch(input);
+    await fetchLocation();
   }
 
   @override
@@ -57,50 +74,76 @@ class _WeatherAppState extends State<WeatherApp> {
               fit: BoxFit.cover,
             ),
           ),
-          child: Scaffold(
-            backgroundColor: Colors.transparent,
-            body: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Center(
-                      child: Text(
-                        temperature.toString() + ' °C',
-                        style: TextStyle(color: Colors.white, fontSize: 60.0),
+          child: temperature == null
+              ? Center(child: CircularProgressIndicator())
+              : Scaffold(
+                  backgroundColor: Colors.transparent,
+                  body: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Center(
+                            child: Image.network(
+                              'https://www.metaweather.com/static/img/weather/png/' +
+                                  abbreviation +
+                                  '.png',
+                              width: 100,
+                            ),
+                          ),
+                          Center(
+                            child: Text(
+                              temperature.toString() + ' °C',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 60.0),
+                            ),
+                          ),
+                          Center(
+                            child: Text(
+                              location,
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 40.0),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                    Center(
-                      child: Text(
-                        location,
-                        style: TextStyle(color: Colors.white, fontSize: 40.0),
+                      Column(
+                        children: <Widget>[
+                          Container(
+                            width: 300,
+                            child: TextField(
+                              onSubmitted: (String input) {
+                                onTextFieldSubmitted(input);
+                              },
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 25),
+                              decoration: InputDecoration(
+                                hintText: 'Search another location...',
+                                hintStyle: TextStyle(
+                                    color: Colors.white, fontSize: 18.0),
+                                prefixIcon:
+                                    Icon(Icons.search, color: Colors.white),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(right: 32.0, left: 32.0),
+                            child: Text(
+                              errorMessage,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Colors.redAccent,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: Platform.isAndroid ? 15.0 : 20.0),
+                            ),
+                          )
+                        ],
                       ),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Container(
-                      width: 300,
-                      child: TextField(
-                        onSubmitted: (String input) {
-                          onTextFieldSubmitted(input);
-                        },
-                        style: TextStyle(color: Colors.white, fontSize: 25),
-                        decoration: InputDecoration(
-                          hintText: 'Search another location...',
-                          hintStyle:
-                              TextStyle(color: Colors.white, fontSize: 18.0),
-                          prefixIcon: Icon(Icons.search, color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          )),
+                    ],
+                  ),
+                )),
     );
   }
 }
