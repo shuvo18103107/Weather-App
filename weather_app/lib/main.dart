2df00db9b29a3222b95b8fae558ef484437eb9f8
shuvo18103107 <18103107@iubat.edu>
2020-11-20 20:53:38 +0600

    Fetch data from internet using API call

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -1,80 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main() => runApp(Weatherapp());
+void main() => runApp(WeatherApp());
 
-class Weatherapp extends StatefulWidget {
+class WeatherApp extends StatefulWidget {
   @override
-  _WeatherappState createState() => _WeatherappState();
+  _WeatherAppState createState() => _WeatherAppState();
 }
 
-class _WeatherappState extends State<Weatherapp> {
+class _WeatherAppState extends State<WeatherApp> {
   int temperature = 0;
-  String location = 'San Fransisco';
+  String location = 'San Francisco';
+  int woeid = 2487956;
+  String weather = 'clear';
+
+  String searchApiUrl =
+      'https://www.metaweather.com/api/location/search/?query=';
+  String locationApiUrl = 'https://www.metaweather.com/api/location/';
+
+  void fetchSearch(String input) async {
+    var searchResult = await http.get(searchApiUrl + input);
+    var result = json.decode(searchResult.body)[0];
+
+    setState(() {
+      location = result["title"];
+      woeid = result["woeid"];
+    });
+  }
+
+  void fetchLocation() async {
+    var locationResult = await http.get(locationApiUrl + woeid.toString());
+    var result = json.decode(locationResult.body);
+    var consolidated_weather = result["consolidated_weather"];
+    var data = consolidated_weather[0];
+
+    setState(() {
+      temperature = data["the_temp"].round();
+      weather = data["weather_state_name"].replaceAll(' ', '').toLowerCase();
+    });
+  }
+
+  void onTextFieldSubmitted(String input) {
+    fetchSearch(input);
+    fetchLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Container(
-        decoration: BoxDecoration(
+          decoration: BoxDecoration(
             image: DecorationImage(
-          image: AssetImage('images/clear.png'),
-          fit: BoxFit.cover,
-        )),
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            // crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Column(
-                children: [
-                  Center(
-                    child: Text(
-                      temperature.toString() + '°C',
-                      style: TextStyle(
-                        fontSize: 60.0,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  Center(
-                    child: Text(
-                      location,
-                      style: TextStyle(
-                        fontSize: 40.0,
-                        color: Colors.white,
-                      ),
-                    ),
-                  )
-                ],
-              ),
-              Column(
-                children: [
-                  Container(
-                    width: 300,
-                    child: TextField(
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25.0,
-                      ),
-                      decoration: InputDecoration(
-                          hintText: 'Search another location..',
-                          hintStyle: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18.0,
-                          ),
-                          prefixIcon: Icon(
-                            Icons.search,
-                            color: Colors.white,
-                          )),
-                    ),
-                  )
-                ],
-              )
-            ],
+              image: AssetImage('images/$weather.png'),
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-      ),
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Center(
+                      child: Text(
+                        temperature.toString() + ' °C',
+                        style: TextStyle(color: Colors.white, fontSize: 60.0),
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        location,
+                        style: TextStyle(color: Colors.white, fontSize: 40.0),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Container(
+                      width: 300,
+                      child: TextField(
+                        onSubmitted: (String input) {
+                          onTextFieldSubmitted(input);
+                        },
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                        decoration: InputDecoration(
+                          hintText: 'Search another location...',
+                          hintStyle:
+                              TextStyle(color: Colors.white, fontSize: 18.0),
+                          prefixIcon: Icon(Icons.search, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
